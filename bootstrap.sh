e2e4e467082cf0a11dc8e4cd7f6a33bb023027b0
Danilo Lima <danilo.lima@grupofolha.com.br>
2016-05-09 18:55:25 -0300

    Removidos dados iniciais

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,36 +15,3 @@ sudo service elasticsearch start
 
 sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
 
-curl -XPOST 'http://localhost:9200/megacorp/employee/1' -d '
-{
-   "first_name": "John",
-   "last_name": "Smith",
-   "age": 25,
-   "about": "I love to go rock climbing",
-   "interests": [
-      "sports",
-      "music"
-   ]
-}'
-
-curl -XPOST 'http://localhost:9200/megacorp/employee/2' -d '
-{
-   "first_name": "Jane",
-   "last_name": "Smith",
-   "age": 32,
-   "about": "I like to collect rock albums",
-   "interests": [
-      "music"
-   ]
-}'
-
-curl -XPOST 'http://localhost:9200/megacorp/employee/3' -d '
-{
-   "first_name": "Douglas",
-   "last_name": "Fir",
-   "age": 35,
-   "about": "I like to build cabinets",
-   "interests": [
-      "forestry"
-   ]
-}'
